bc4536eef9b0fc5859c674dd1fa304fdfcceb715
Raksha Pawar <rakshapawar10.sae@gmail.com>
2022-10-16 21:04:41 +0530

    feat: add logic to button to move the stepper

--- React-JS Projects/multi-step-form/src/App.jsx
+++ React-JS Projects/multi-step-form/src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from "react";
 import "./App.css";
 import { Stepper, StepperControls } from "./components";
-import { AccountSetup, PersonalDetails, Complete } from "./pages";
-import {steps} from './utils'
+import { AccountSetup, Complete, PersonalDetails } from "./pages";
+import { steps } from "./utils";
 
 function App() {
   const [currentStep, setCurrentStep] = useState(1);
+  const handleClick = (direction) => {
+    let newStep = currentStep;
+
+    direction === "next" ? newStep++ : newStep--;
+
+    newStep > 0 && newStep <= steps.length && setCurrentStep(newStep);
+  };
   const displayStep = (step) => {
     switch (step) {
       case 1:
@@ -22,7 +29,11 @@ function App() {
       <div className="container horizontal mt-5">
         <Stepper steps={steps} currentStep={currentStep} />
       </div>
-      <StepperControls />
+      <StepperControls
+        handleClick={handleClick}
+        currentStep={currentStep}
+        steps={steps}
+      />
     </div>
   );
 }
