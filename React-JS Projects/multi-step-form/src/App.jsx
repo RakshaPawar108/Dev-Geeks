d3dceb49a8e8edc02cba864ec0d0302f152b3fba
Raksha Pawar <rakshapawar10.sae@gmail.com>
2022-10-23 19:33:42 +0530

    feat: add form fields to all pages

--- React-JS Projects/multi-step-form/src/App.jsx
+++ React-JS Projects/multi-step-form/src/App.jsx
@@ -26,7 +26,7 @@ function App() {
     }
   };
   return (
-    <div className="App md:w-1/2 mx-auto shadow-xl rounded-2xl pb-2 bg-white">
+    <div className="md:w-1/2 mx-auto shadow-xl rounded-2xl pb-2 bg-white">
       <div className="container horizontal mt-5">
         <Stepper steps={steps} currentStep={currentStep} />
 
